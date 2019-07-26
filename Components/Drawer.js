97ff4300142e4df54a30e2ecad4204e016858b9a
Choejiwon <jiwon0929@ajou.ac.kr>
2019-07-26 23:29:46 +0900

    Drawer Update

--- Components/Drawer.js
+++ Components/Drawer.js
@@ -26,7 +26,7 @@ class HamburgerIcon extends Component {
                 <TouchableOpacity onPress={this.toggleDrawer.bind(this)} >
 
                     <Image
-                        source={{ uri: 'https://reactnativecode.com/wp-content/uploads/2018/04/hamburger_icon.png' }}
+                        source={require('../Icon/menu_icon.png')}
                         style={{ width: 25, height: 25, marginLeft: 5 }}
                     />
 
@@ -40,6 +40,7 @@ class HamburgerIcon extends Component {
     }
 }
 
+//합치고 싶을 경우에는 이 메인 액티비티에다가 Diary_list내용 집어넣으면 됨!
 class MainActivity extends Component {
 
     constructor(props) {
@@ -65,114 +66,50 @@ class MainActivity extends Component {
     }
 }
 
-class SecondActivity extends Component {
-
-    constructor(props) {
-
-        super(props);
-
-        YellowBox.ignoreWarnings([
-            'Warning: componentWillMount is deprecated',
-            'Warning: componentWillReceiveProps is deprecated',
-        ]);
-
-    }
-
-    render() {
-        return (
-
-            <View style={styles.MainContainer}>
-
-                <Text style={{ fontSize: 23 }}> This is Activity - 2 </Text>
-
-            </View>
-        );
-    }
-}
-
-class ThirdActivity extends Component {
-
-    constructor(props) {
-
-        super(props);
-
-        YellowBox.ignoreWarnings([
-            'Warning: componentWillMount is deprecated',
-            'Warning: componentWillReceiveProps is deprecated',
-        ]);
-
-    }
-
-    render() {
-        return (
-
-            <View style={styles.MainContainer}>
-
-                <Text style={{ fontSize: 23 }}> This is Activity - 3 </Text>
-
-            </View>
-        );
-    }
-}
-
-const FirstActivity_StackNavigator = createStackNavigator({
+const Diary_list_StackNavigator = createStackNavigator({
     First: {
         screen: Diary_list,
         navigationOptions: ({ navigation }) => ({
             title: 'Diary_list',
             headerLeft: <HamburgerIcon navigationProps={navigation} />,
-
-            headerStyle: {
-                backgroundColor: '#FF9800'
-            },
-            headerTintColor: '#fff',
         })
     },
 });
 
 
-const SecondActivity_StackNavigator = createStackNavigator({
+const Nearby_attractions_StackNavigator = createStackNavigator({
     Second: {
         screen: Nearby_attractions,
         navigationOptions: ({ navigation }) => ({
-            title: 'Nearby_attractions',
+            title: '주변 관광 명소',
             headerLeft: <HamburgerIcon navigationProps={navigation} />,
-
-            headerStyle: {
-                backgroundColor: '#FF9800'
-            },
-            headerTintColor: '#fff',
         })
     },
 });
 
 
-const ThirdActivity_StackNavigator = createStackNavigator({
+const Path_search_StackNavigator = createStackNavigator({
     Third: {
         screen: Path_search,
         navigationOptions: ({ navigation }) => ({
-            title: 'Path_search',
+            title: '길 찾기',
             headerLeft: <HamburgerIcon navigationProps={navigation} />,
-
-            headerStyle: {
-                backgroundColor: '#FF9800'
-            },
-            headerTintColor: '#fff',
         })
     },
 });
 
 export default MyDrawerNavigator = createDrawerNavigator({
-    Diary_list: {
-        screen: FirstActivity_StackNavigator
+    //헤더 설정해서 나중에 여기다가 계정이나 그런거 표시해도 좋을듯.
+    Diary_list: {   //메뉴에 나타나는 이름
+        screen: Diary_list_StackNavigator
     },
 
-    Nearby_attractions: {
-        screen: SecondActivity_StackNavigator
+    "주변 관광 명소": { 
+        screen: Nearby_attractions_StackNavigator
     },
 
-    Path_search: {
-        screen: ThirdActivity_StackNavigator
+    "길찾기": {
+        screen: Path_search_StackNavigator
     }
 });
 
