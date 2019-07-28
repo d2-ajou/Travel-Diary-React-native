21504dde50e121e1a02d7d6fdc7e277b8eb155a6
dawoon <rhdrlvnd@gmail.com>
2019-07-28 23:32:07 +0900

    image picker inprogress...

--- Components/Write_diary.js
+++ Components/Write_diary.js
@@ -1,5 +1,29 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
+
+ImagePicker.showImagePicker((response) => {
+  console.log('Response = ', response);
+
+  if (response.didCancel) {
+    console.log('User cancelled image picker');
+  } else if (response.error) {
+    console.log('ImagePicker Error: ', response.error);
+  } else if (response.customButton) {
+    console.log('User tapped custom button: ', response.customButton);
+  } else {
+    const source = { uri: response.uri };
+
+    // You can also display the image using data:
+    // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+    this.setState({
+      avatarSource: source,
+    });
+  }
+})
+
 
 export default class Write_diary extends Component {
   render() {
@@ -12,15 +36,23 @@ export default class Write_diary extends Component {
           사실 우리가 디자인한 그림에서는 다이어리 리스트에서 다이어리 추가가 가능하기에
           이 페이지는 없어도 될듯?
         </Text>
+        <Image source={this.state.avatarSource} style={styles.uploadAvatar} />
       </View>
     );
   }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ImagePicker.launchCamera((response) => {
 });
+
+ImagePicker.launchImageLibrary((response) => {
+});
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
