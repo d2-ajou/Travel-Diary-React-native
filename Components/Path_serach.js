67548549d7148339270e71ed31c36d1425c6d477
do02reen24 <do02reen24@gmail.com>
2019-07-29 20:30:23 +0900

    path search delete not necessary code

--- Components/Path_serach.js
+++ Components/Path_serach.js
@@ -7,14 +7,6 @@ export default class Path_search extends Component {
 
   handleGetDirections = () => {
     const data = {
-      //  source: {
-      //   latitude: -33.8356372,
-      //   longitude: 18.6947617
-      // },
-      // destination: {
-      //   latitude: -33.8600024,
-      //   longitude: 18.697459
-      // },
       params: [
         {
           key: "travelmode",
@@ -24,26 +16,7 @@ export default class Path_search extends Component {
           key: "dir_action",
           value: "navigate"       // this instantly initializes navigation using the given travel mode
         }
-      ]//,
-      // waypoints: [
-      //   {
-      //     latitude: -33.8600025,
-      //     longitude: 18.697452,
-      //   },
-      //   {
-      //     latitude: -33.8600026,
-      //     longitude: 18.697453,
-      //   },
-      //      {
-      //     latitude: -33.8600036,
-      //     longitude: 18.697493,
-      //   },
-      //      {
-      //     latitude: -33.8600046,
-      //     longitude: 18.69743,
-      //   },
-
-      // ]
+      ]
     }
 
     getDirections(data)
